42167b882fd6bb442ea3ddac31e40f3c26acca4e
baggerest <baggerest@yahoo.com.tw>
2017-03-17 09:05:55 +0800

    ht.txt

--- index.php
+++ index.php
@@ -6,12 +6,12 @@
  * Time: 下午 02:52
  */
 
-// <table class="auto-style1"> - 開始讀取
+// <td style="background-color: #B02D00; color: #FFFFFF; font-size: 32px; font-weight: bold;text-align:center">特別號</td> - 開始讀取
 // <ins class="clickforceads" style="display:inline-block;width:728px;height:90px" data-ad-zone="942"></ins> - 讀取結束
 //共61頁
 // http://www.pilio.idv.tw/ltobig/list.asp?indexpage=1 - 網址
 function get_url_list(){
-    $start = '<table class="auto-style1">';
+    $start = '<td style="background-color: #B02D00; color: #FFFFFF; font-size: 32px; font-weight: bold;text-align:center">特別號</td>';
     $end = '<ins class="clickforceads" style="display:inline-block;width:728px;height:90px" data-ad-zone="942"></ins>';
     $start_page = 1;
     $end_page = 61;
@@ -25,6 +25,7 @@ function get_url_list(){
             if($read===false){
                 if($value===$start){
                     $read = true;
+                    continue;
                 }
             }else{
                 if($value===$end){
@@ -38,9 +39,21 @@ function get_url_list(){
     fclose($f_w);
 }
 
-function get_number_list(){
+$replace_list = array(
+    '</tr>',
+    '<tr style="text-align:center; background-color: #FFDBCE;">',
+    '<td style="font-size: 32px; font-weight: bold; color: #000000;border-bottom-style: dotted; border-bottom-color: #CCCCCC">',
+    '<td style="font-size: 48px; font-weight: bold; color: #000000;border-bottom-style: dotted; border-bottom-color: #CCCCCC; word-break: break-all">',
+    '</td>',
+    '<td style="font-size: 48px; font-weight: bold; color: #000000;border-bottom-style: dotted; border-bottom-color: #CCCCCC">',
+    '<tr style="text-align:center; ">',
+    '</table>'
+);
+get_number_list($replace_list);
+function get_number_list($list = array()){
     $f = file("ht.txt",FILE_IGNORE_NEW_LINES|FILE_SKIP_EMPTY_LINES);
     foreach ($f as $value){
-
+        $value = str_replace($list,'',$value);
+        echo $value.'<br>';
     }
 }
